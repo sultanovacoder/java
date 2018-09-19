08f4d0c8d1e78009d5c279fce74cd133a4dc215e
Маря <sultanova.coder@gmail.com>
2018-09-19 22:46:12 +0300

    4-я задача готова

--- less2.java
+++ less2.java
@@ -10,6 +10,10 @@ public class less2 {
 		makeRev(30);
 		System.out.printf("%n %n");
 		fillArray();
+		System.out.printf("%n %n");
+		multiplyTwo();
+		System.out.printf("%n %n");
+		fillDiagonal();
 	}
 	
 	public static void makeRev(int a){
@@ -28,8 +32,7 @@ public class less2 {
 	
 	public static void fillArray(){
 		int[] arr = new int[8];
-		int j = 0; 
-		int i = 0;
+		int j = 0, i = 0;
 		while (i < arr.length) {
 			arr[i] = j;
 			System.out.print(arr[i] + " ");
@@ -37,4 +40,28 @@ public class less2 {
 			i++;
 		}
 	}
+	
+	public static void multiplyTwo(){
+		int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < 6) arr[i] *= 2;
+			System.out.print(arr[i] + " ");
+		}
+	}
+	
+	public static void fillDiagonal(){
+		int[][] arr = new int[10][10];
+		for (int i = 0; i < arr.length; i++){
+			int perv = i;
+			int posl = arr.length-1-i;
+			arr[perv][perv] = 1;
+			arr[perv][posl] = 1;
+			
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "   ");
+			}
+			System.out.printf("%n");
+		}
+	}
+	
 }
