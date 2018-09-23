ddb9abdd3b14b54eb7e6d662876e57487d5a0c05
Маря <sultanova.coder@gmail.com>
2018-09-24 02:45:05 +0300

    описан метод создания поля

--- less4.java
+++ less4.java
@@ -5,3 +5,23 @@
 * @version dated Sep 24, 2018
 */
 
+public class less4 {
+    int k = 0; //количество ходов человека
+    
+    public static void main(String[] args) {
+        createField();
+    }
+    
+    public static void createField() {	
+        char[][] table = new char[3][3];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = '.';
+                System.out.print(table[i][j] + "    ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void walksMan() {	
+}
