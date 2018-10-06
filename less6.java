3c15bb9a7134210e7bb92af6aa820022025b40ef
Маря <sultanova.coder@gmail.com>
2018-10-06 22:56:04 +0300

    импортировала сканер, теперь животное плавает и прыгает, бегает на указанную дистанцию

--- less6.java
+++ less6.java
@@ -2,24 +2,33 @@
 * Java. Level 1. Lesson 6
 *
 * @author Marina Sultanova 
-* @version dated Sep 26, 2018
+* @version dated Oct 07, 2018
 */
 
-
+import java.util.Scanner;
 
 public class less6 {
     public static void main(String[] args) {
         Cat cat = new Cat();
-        System.out.println(cat.runAnimals());
+        Scanner sc = new Scanner(System.in);
+        System.out.println(cat.run(sc.nextInt()));
+        /*System.out.println(cat.jump());
+        System.out.println(cat.swim());*/
     }
 }
 
 class Animals{
     protected String a = " animal";
-    public String runAnimals() {
-        return (a + " run");
+    public String run(int r) {
+        return (a + " to run " + r + " distance");
+    }
+    public String swim(int r){
+        return (a + " to swim " + r + " distance");
     }
     
+    public String jump(int r){
+        return (a + " to jump " + r + " distance");
+    }
 }
 
 class Cat extends Animals{
