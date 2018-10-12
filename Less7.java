bb85ac3a92d935e8f3e6273554e0eb9c357e60cf
Маря <sultanova.coder@gmail.com>
2018-10-12 23:57:42 +0300

    теперь массив котов жрет из тарелки

--- Less7.java
+++ Less7.java
@@ -9,32 +9,43 @@ import java.util.Scanner;
 
 public class Less7{
     public static void main(String[] args){
-         Cat cat = new Cat(10);
+        
+         Cat[] cats = new Cat[5];
+         cats[0] = new Cat("Tigrenia",10);
+         cats[1] = new Cat("Barsik",2);         
+         cats[2] = new Cat("Gnom",5);
+         cats[3] = new Cat("Shnyrok",3);
+         cats[4] = new Cat("Mikki",3);      
+         
          Plate plate = new Plate(12);
          Scanner sc = new Scanner(System.in);
-         System.out.println("koty nyghno s'est " + cat.fill);
-         System.out.println("sejchas edi v tarelke " + plate.volume);
-         cat.eat(plate);
-         System.out.println("kolichestvo edi v tarelkre posle togo kak poel kot " + plate.volume);
-         System.out.println("dly togo chtob naests'a koty nyghno eshe " + cat.fill + " porcij");
-         System.out.println("Dobavit korma? 1 - da; 2-net");
-         if (sc.nextInt() == 1) {
-             plate.addEat(sc.nextInt());
-         } else {
-             return;
+         for (Cat cat:cats) {
+            System.out.println(cat.name + " koty nyghno s'est " + cat.fill);
+            System.out.println("sejchas edi v tarelke " + plate.volume);
+            cat.eat(plate);
+            System.out.println("kolichestvo edi v tarelkre posle togo kak poel kot " + plate.volume);
+            System.out.println("dly togo chtob naests'a koty nyghno eshe " + cat.fill + " porcij");
+            System.out.println("Dobavit korma? 1 - da; 2-net");
+            if (sc.nextInt() != 1) {
+                continue;
+            }
+            plate.addEat(sc.nextInt());
+            System.out.println("sejchas edi v tarelke " + plate.volume);
          }
-         System.out.println("sejchas edi v tarelke " + plate.volume);
-        // System.out.println(plate);         
+         for (Cat cat:cats){
+             System.out.println(cat.name + cat.fill);
+         }  
     }
 }
 
 class Cat {
-    String name = "Tigrenia";
+    String name;
     int fill;
     boolean appetite; 
     
-    public Cat(int x){
-        fill = x;
+    public Cat(String x, int y){
+        name = x;
+        fill = y;
     }
     
     public void eat(Plate plate){
@@ -45,15 +56,15 @@ class Cat {
 class Plate{
     int volume;
     public Plate(int x){
-        this.volume = x; //здесь же this не нужно?!
+        this.volume = x; //здесь же this не нужно! потому что я аргумент обозвала иксом
     }
     
     public void minysEdiIzTarelki(Cat cat) {
-        if (cat.fill >= this.volume) {
-            cat.fill = cat.fill - this.volume;
-            this.volume = 0;
+        if (cat.fill >= volume) {
+            cat.fill = cat.fill - volume;
+            volume = 0;
         } else {
-            this.volume = this.volume - cat.fill;
+            volume = volume - cat.fill;
             cat.fill = 0;
         }
     }
