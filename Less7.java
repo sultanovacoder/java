ed6e81d952ff3472cd9af91de0cae4076293e96a
Маря <sultanova.coder@gmail.com>
2018-10-12 23:25:41 +0300

    метод добавления корма в тарелку

--- Less7.java
+++ Less7.java
@@ -5,15 +5,25 @@
 * @version dated Oct 12, 2018
 */
 
+import java.util.Scanner;
+
 public class Less7{
     public static void main(String[] args){
-         Cat cat = new Cat(12);
-         Plate plate = new Plate(10);
+         Cat cat = new Cat(10);
+         Plate plate = new Plate(12);
+         Scanner sc = new Scanner(System.in);
          System.out.println("koty nyghno s'est " + cat.fill);
          System.out.println("sejchas edi v tarelke " + plate.volume);
          cat.eat(plate);
          System.out.println("kolichestvo edi v tarelkre posle togo kak poel kot " + plate.volume);
          System.out.println("dly togo chtob naests'a koty nyghno eshe " + cat.fill + " porcij");
+         System.out.println("Dobavit korma? 1 - da; 2-net");
+         if (sc.nextInt() == 1) {
+             plate.addEat(sc.nextInt());
+         } else {
+             return;
+         }
+         System.out.println("sejchas edi v tarelke " + plate.volume);
         // System.out.println(plate);         
     }
 }
@@ -29,12 +39,7 @@ class Cat {
     
     public void eat(Plate plate){
         plate.minysEdiIzTarelki(this);
-        //this.fill = this.fill - plate.volume;
     }
-    
-    /*public int wantEat(int x) {
-        return this.fill = x * (-1);
-    }*/
 }
 
 class Plate{
@@ -47,11 +52,13 @@ class Plate{
         if (cat.fill >= this.volume) {
             cat.fill = cat.fill - this.volume;
             this.volume = 0;
-            
         } else {
             this.volume = this.volume - cat.fill;
             cat.fill = 0;
         }
     }
     
+    public void addEat(int x) {
+        volume += x;
+    }
 }
