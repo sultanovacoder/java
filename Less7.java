4242018f8fe78be361e07850cf0231007159e251
Маря <sultanova.coder@gmail.com>
2018-10-12 22:20:41 +0300

    прописан метод отнимающий еду из тарелки

--- Less7.java
+++ Less7.java
@@ -7,36 +7,44 @@
 
 public class Less7{
     public static void main(String[] args){
-         Cat cat = new Cat();
-         Plate plate = new Plate(1);
+         Cat cat = new Cat(4);
+         Plate plate = new Plate(5);
          System.out.println("koty nyghno s'est " + cat.fill);
          System.out.println("sejchas edi v tarelke " + plate.volume);
          cat.eat(plate);
-         //System.out.println("kolichestvo edi v tarelkre posle togo kak poel kot " + plate.volume);
-         //System.out.println("dly togo chtob naests'a koty nyghno eshe " + cat.fill + " porcij");*/
+         System.out.println("kolichestvo edi v tarelkre posle togo kak poel kot " + plate.volume);
+         //System.out.println("dly togo chtob naests'a koty nyghno eshe " + cat.fill + " porcij");
         // System.out.println(plate);         
     }
 }
 
 class Cat {
     String name = "Tigrenia";
-    int fill = 4;
+    int fill;
+    boolean appetite; 
+    
+    public Cat(int x){
+        fill = x;
+    }
     
     public void eat(Plate plate){
-        //plate.minysEdiIzTarelki(this.fill);
+        plate.minysEdiIzTarelki(fill);
+        //this.fill = this.fill - plate.volume;
     }
     
-    public int wantEat(int x) {
+    /*public int wantEat(int x) {
         return this.fill = x * (-1);
-    }
+    }*/
 }
 
 class Plate{
     int volume;
     public Plate(int x){
-        volume = x; //здесь же this не нужно?!
+        this.volume = x; //здесь же this не нужно?!
     }
-    /*public int minysEdiIzTarelki(int fill) {
-
-    }*/
+    
+    public void minysEdiIzTarelki(int fill) {
+        this.volume = this.volume - fill;
+    }
+    
 }
